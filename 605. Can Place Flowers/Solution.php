d87836af3db490c653e904ee1a92907f62a76803
marcelo <marcelopeixoto314@gmail.com>
2025-08-27 23:47:56 -0300

    refeito com php

---
+++ 605. Can Place Flowers/Solution.php
@@ -0,0 +1,27 @@
+class Solution {
+
+    /**
+     * @param Integer[] $flowerbed
+     * @param Integer $n
+     * @return Boolean
+     */
+    function canPlaceFlowers($flowerbed, $n) {
+        $count = 0;
+        for($i = 0; $i < sizeof($flowerbed); $i++){
+            if($i==0){
+                if($flowerbed[$i+1] == 0 && $flowerbed[$i] == 0){
+                    $flowerbed[$i] = 1;
+                    $count++;
+                }
+            }else if( $i==sizeof($flowerbed) && $flowerbed[$i-1] == 0 && $flowerbed[$i] == 0){
+                $flowerbed[$i] = 1;
+                $count++;
+            }else if($flowerbed[$i-1] == 0 && $flowerbed[$i+1] == 0 && $flowerbed[$i] == 0){
+                $flowerbed[$i] = 1;
+                $count++;
+            }
+        }
+
+        return $count >= $n;
+    }
+}

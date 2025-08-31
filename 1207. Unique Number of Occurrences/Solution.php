b197fc1ba26bc98b63607f732b1f255b39b8b881
marcelo <marcelopeixoto314@gmail.com>
2025-08-30 22:10:23 -0300

    Exercicio 1207

---
+++ 1207. Unique Number of Occurrences/Solution.php
@@ -0,0 +1,27 @@
+<?php
+class Solution {
+
+    /**
+     * @param Integer[] $arr
+     * @return Boolean
+     */
+    function uniqueOccurrences($arr) {
+        $uniqueValues = array_unique($arr);
+        $countArr = array();
+        foreach ($uniqueValues as $atual) {
+            $count = 0;
+            for ($j = 0; $j < sizeof($arr); $j++) {
+                if ($arr[$j] === $atual) {
+                    $count++;
+                }
+            }
+            $countArr[] = $count;
+        }
+
+        $totalCount = count($countArr);
+        $uniqueCount = count(array_unique($countArr));
+
+        return $totalCount == $uniqueCount;
+    }
+}
+?>
